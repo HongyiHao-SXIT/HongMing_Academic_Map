de0d53f8470f0186e2e4d9bbfca7eada1a2d9bad
HongyiHao-SXIT <Lanyi_adict@outlook.com>
2025-06-26 14:11:38 +0800

    last code

---
+++ src/main/java/com/hm_map/hm_map/entity/User.java
@@ -0,0 +1,28 @@
+package com.hm_map.hm_map.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "users")
+public class User {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    private String email;
+
+    private String school;
+
+    private LocalDateTime registerTime;
+}
